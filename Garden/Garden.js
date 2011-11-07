07b4ab7514334624305f61395dcc0ae8d729b5db
Nat Budin <natbudin@gmail.com>
2011-11-07 07:34:43 -0500

    Multiple playthrough setup (no backend support yet), act/scene counter

--- Garden/Garden.js
+++ Garden/Garden.js
@@ -31,41 +31,59 @@ var actBreak = function() {
     }
   }
 }
+
+var GameState = { scene: 1, act: 1 };
+
+function SetScene(newAct, newScene) {
+  return function() {
+    GameState.act = newAct;
+    GameState.scene = newScene;
+  };
+}
   
 // Game rules
 with ( TraverseDSL ) {
-  Choice( "Barbara Marries Somebody", "Barbara Marries Charles", "Barbara Marries William" );
+  Choice( "Barbara Marries Somebody", "Barbara Marries Charles", "Barbara Marries William",
+    AndThen(SetScene(1, 2)));
 
   After( "Barbara Marries Somebody",
-    Choice( "Barbara Chooses Whether to Cheat", "Barbara Cheats", "Barbara Doesn't Cheat" ));
+    Choice( "Barbara Chooses Whether to Cheat", "Barbara Cheats", "Barbara Doesn't Cheat" ),
+    AndThen(SetScene(1, 3), OnlyIf(EitherOf("Barbara Marries William", "Charles Chooses Whether to Cheat"))));
   After( "Barbara Marries Charles",
-    Choice( "Charles Chooses Whether to Cheat", "Charles Cheats", "Charles Doesn't Cheat" ));
+    Choice( "Charles Chooses Whether to Cheat", "Charles Cheats", "Charles Doesn't Cheat" ),
+    AndThen(SetScene(1, 3), OnlyIf("Barbara Chooses Whether to Cheat")));
     
   After( 
     BothOf( 
       "Barbara Chooses Whether to Cheat", 
       EitherOf( "Barbara Marries William", "Charles Chooses Whether to Cheat" )
     ),
-    Choice( "They Choose Whether to Divorce", "They Divorce", "They Stay Together" ));
+    Choice( "They Choose Whether to Divorce", "They Divorce", "They Stay Together" ),
+    AndThen(SetScene(1, 4)));
 
   After( "They Choose Whether to Divorce",    
     Choice( "They Decide What to Do With Virginia", "Virginia Moves In With the Couple", "They Put Virginia in a Nursing Home" ),
-    AndThen( actBreak ));
+    AndThen( actBreak, SetScene(2, 1) ));
   
   After( BothOf( "They Decide What to Do With Virginia", "Barbara Marries William" ),
-    Choice( "Charles Decides Whether to Lend Barbara Money", "Charles Lends Her Money", "Charles Doesn't Lend Her Money" ));
+    Choice( "Charles Decides Whether to Lend Barbara Money", "Charles Lends Her Money", "Charles Doesn't Lend Her Money" ),
+    AndThen(SetScene(2, 2)));
   After( AllOf( "They Decide What to Do With Virginia", "Barbara Marries Charles", "They Divorce" ),
-    Choice( "William Decides Whether to Lend Barbara Money", "William Lends Her Money", "William Doesn't Lend Her Money" ));
+    Choice( "William Decides Whether to Lend Barbara Money", "William Lends Her Money", "William Doesn't Lend Her Money" ),
+    AndThen(SetScene(2, 2)));
   After( AllOf( "They Decide What to Do With Virginia", "Barbara Marries Charles", "They Stay Together" ),
-    Choice( "William Decides Whether to Lend Them Money", "William Lends Them Money", "William Doesn't Lend Them Money" ));
+    Choice( "William Decides Whether to Lend Them Money", "William Lends Them Money", "William Doesn't Lend Them Money" ),
+    AndThen(SetScene(2, 2)));
 
   After( AnyOf( "Charles Decides Whether to Lend Barbara Money", 
                 "William Decides Whether to Lend Barbara Money",
                 "William Decides Whether to Lend Them Money" ),
-         Choice( "They Decide Where to Send Stephanie", "They Send Stephanie to Rehab", "They Send Stephanie to Jail" ));
+         Choice( "They Decide Where to Send Stephanie", "They Send Stephanie to Rehab", "They Send Stephanie to Jail" ),
+         AndThen(SetScene(2, 3)));
 
   After( "They Decide Where to Send Stephanie",
-    Choice( "They Meet Jason", "They Accept Jason", "They Reject Jason" ));
+    Choice( "They Meet Jason", "They Accept Jason", "They Reject Jason" ),
+    AndThen(SetScene(2, 4)));
       
   After( "They Meet Jason",
     Choice( "Jason and Stephanie Decide What to Do With Their Parents",
@@ -100,7 +118,7 @@ var Action = Backbone.Model.extend({
   
   run: function() {
     this.traverseAction.Run();
-    this.playthrough.trigger("eligibleActionsChange");
+    this.playthrough.trigger("action");
   }
 });
 
@@ -110,7 +128,10 @@ var Playthrough = Backbone.Model.extend({
   },
   
   pastChoices: function() {
-    return _.values(TraverseCore.ChoiceResults);
+    return _.keys(TraverseCore.ChoiceResults).filter(function (choice) {
+      // only return ones that are true in the results table
+      return TraverseCore.ChoiceResults[choice];
+    });
   },
   
   eligibleActions: function() {
@@ -144,7 +165,7 @@ var EligibleActionsView = Backbone.View.extend({
   className: "choices",
   
   initialize: function() {
-    this.model.bind("eligibleActionsChange", this.render, this);
+    this.model.bind("action", this.render, this);
   },
   
   render: function() {
@@ -162,7 +183,74 @@ var EligibleActionsView = Backbone.View.extend({
   }
 });
 
+var PastChoicesView = Backbone.View.extend({
+
+  tagName: "ul",
+  className: "pastChoices",
+  
+  initialize: function() {
+    this.model.bind("action", this.render, this);
+  },
+  
+  render: function() {
+    $(this.el).empty();
+    
+    _.each(this.model.pastChoices(), function(choice) {
+      $(this.el).append("<li>" + choice + "</li>");
+    }, this);
+    
+    return this;
+  }
+  
+});
+
+var SceneCounterView = Backbone.View.extend({
+
+  tagName: "div",
+  className: "sceneCounter",
+  
+  initialize: function() {
+    this.model.bind("action", this.render, this);
+  },
+  
+  render: function() {
+    $(this.el).html("Act " + GameState.act + ", Scene " + GameState.scene);
+  }
+});
+
+var PlaythroughView = Backbone.View.extend({
+  
+  tagName: "div",
+  className: "playthrough",
+
+  initialize: function() {
+    this.sceneCounterView = new SceneCounterView({ 
+      model: this.model,
+      el: this.$('.sceneCounter')
+    });
+    this.eligibleActionsView = new EligibleActionsView({ 
+      model: this.model,
+      el: this.$('.choices')
+    });
+    this.pastChoicesView = new PastChoicesView({ 
+      model: this.model,
+      el: this.$('.pastChoices')
+    });
+  },
+  
+  render: function() {
+    var subviews = [this.sceneCounterView, this.eligibleActionsView, this.pastChoicesView];
+    _.each(subviews, function(subview) { subview.render(); });
+    
+    return this;
+  }
+  
+});
+
 $(function() {
   var playthrough = new Playthrough();
-  new EligibleActionsView({ model: playthrough, el: $('.choices').get(0) }).render();
+  $('.playthrough').each(function() {
+    var playthrough = new Playthrough();
+    new PlaythroughView({ model: playthrough, el: this }).render();
+  });
 });
